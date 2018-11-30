d87970e8ca2f18a6a1f938c8b20f97195eb72b97
vic-lee <viclee@nyu.edu>
2018-11-29 19:44:20 -0500

    Add algorithm description

--- short_google_trend.py
+++ short_google_trend.py
@@ -2,6 +2,36 @@ import numpy as np
 import datetime
 from zipline.api import order, record, symbol
 
+'''
+Algorithm description: 
+
+This algorithm shorts the stock market based on how frequent negative keywords 
+are searched on Google.
+
+Detailed Algorithm:
+
+Prior-week search frequency
+Avg search frequency (look-back window or full history)
+
+- every Monday
+  - if prior-week search frequency > avg search frequency
+    - short asset
+  - if prior-week search frequency < avg search frequency
+    - long asset
+- every Friday
+  - close all positions
+
+Variations on algorithm:
+1. look back window for avg search frequency
+2. starting capital
+3. general index / industry-specific indices paired 
+    with industry-specific search terms
+
+Note: 
+Only use search query data at (t - 1) to avoid look-ahead bias.
+
+'''
+
 def initialize(context):
 	context.AAPL = symbol('AAPL')
 	pass
